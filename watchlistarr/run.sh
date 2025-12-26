ec071d7227da033a379ffaab6b42f23b8fcc6554
sledour <103637883+sledour@users.noreply.github.com>
2025-12-26 17:00:40 +0100

    Add run.sh script for Watchlistarr configuration

---
+++ watchlistarr/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+
+# Récupération des options de HA
+export PLEX_TOKEN=$(bashio::config 'PLEX_TOKEN')
+export PLEX_SERVER=$(bashio::config 'PLEX_SERVER')
+export RADARR_URL=$(bashio::config 'RADARR_URL')
+export RADARR_API_KEY=$(bashio::config 'RADARR_API_KEY')
+export SONARR_URL=$(bashio::config 'SONARR_URL')
+export SONARR_API_KEY=$(bashio::config 'SONARR_API_KEY')
+export INTERVAL=$(bashio::config 'INTERVAL')
+
+echo "Lancement de Watchlistarr..."
+# Commande pour démarrer l'application originale
+/app/watchlistarr
