e3ef75d930c3ea8b1d966480b196880d6f46d5a4
sledour <sebastien.ledour@gmail.com>
2026-01-04 18:42:02 +0100

    fix

---
+++ gift/frontend/app/dashboard/settings/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>Événements</h1>
+            <p>Ici s'affichera la liste des événements (Noël, Anniversaires, etc.)</p>
+        </div>
+    );
+}
