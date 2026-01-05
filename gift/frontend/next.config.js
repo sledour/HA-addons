cbb5fb9fa8dee75cd5d32579a7ae8b1b366a07ae
sledour <103637883+sledour@users.noreply.github.com>
2026-01-05 09:35:56 +0100

    Delete gift directory

--- gift/frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
-  // Force Next.js à ne jamais utiliser de "/" au début des liens
-  assetPrefix: './', 
-}
-module.exports = nextConfig
